c3923dc595f9c314c0e7cdf344c546fea3f79922
jo744669 <jo744669@sju.edu>
2024-03-19 16:30:13 -0400

    Updates for testing

--- CapstoneSpring.cpp
+++ CapstoneSpring.cpp
@@ -10,6 +10,244 @@ using namespace std;
 
 int currentScene; //global variable to hold what scene user is at
 
+
+/*
+* This method will be used to find the colors of the different decision options
+*/
+int calibrate(int xOne, int yOne, int xTwo, int yTwo, Vec3b& oneColor, Vec3b& twoColor, Mat image)
+{
+    //uses reference variables for  colors to edit the color variables in main method without returning multiple values
+    //assigns RGB value at location of decision to color vector
+    oneColor = image.at<Vec3b>(yOne, xOne);
+    twoColor = image.at<Vec3b>(yTwo, xTwo);
+    cout << "calibrate: " << oneColor << " and " << twoColor << endl;
+    return 1;
+}
+
+/*
+* This method checks if the vector is empty so that this code does not take up space in the main method each time it needs to be used
+*/
+bool vector_is_empty(Vec3b vector)
+{
+    if (vector[0] == 0)
+    {
+        if (vector[1] == 0)
+        {
+            if (vector[2] == 0)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/*
+* This method will be used to find and display the image the program should be looking for.
+* This method should be called in the beginning for the start scene and for the pause scenes when waiting for user's decision
+* @args imageName = name of image file to look for with .jpg extension
+* @args wDisplay1
+*/
+int getImage(String imageName, int wDisplay1)
+{
+    //import image from file folder 
+    Mat img = imread(imageName, IMREAD_COLOR);
+    cout << img.size << endl;
+
+    //set height and width to cols and rows respectively
+    int hStory = img.cols;
+    int wStory = img.rows;
+
+    if (img.empty())
+    {
+        cout << "Could not read the image: " << imageName << endl;
+        return -1;
+    }
+
+    //display the image to the correct window
+    namedWindow("story", WINDOW_NORMAL);
+    moveWindow("story", wDisplay1, 0); //position display
+    setWindowProperty("story", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+    imshow("story", img); //displays the image
+
+    delete& img; //delete to avoid memory leaks
+    return 0;
+}
+
+/*
+* This method will be used to display the video of the correct scene
+*/
+int playVideo(String videoName, int wDisplay1)
+{
+    VideoCapture cap(videoName);
+    // Check if camera opened successfully
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    //display video to the correct window
+    namedWindow("video", WINDOW_NORMAL);
+    moveWindow("video", wDisplay1, 0); //position display
+    setWindowProperty("video", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+    Mat frame;
+
+    while (1)
+    {
+        // Capture frame-by-frame
+        cap >> frame;
+
+        // If the frame is empty, break immediately
+        if (frame.empty())
+            break;
+
+        // Display the resulting frame
+        imshow("video", frame);
+    }
+
+    delete& frame;
+    cap.release();
+    return 0;
+    //DO I NEED TO CALL GET IMAGE VARIABLE HERE
+}
+
+/*
+* This method will be used to detect if the user has made their decision in a given scene
+* @args xOne, yOne - the x and y coordinates of option 1
+* @args xTwo, yTwo - the x and y coordinates of option 2
+* @args oneColor, twoColor - the original colors of option 1 and 2 determined in calibration step 2
+* @args image - the current image displayed on the screen
+*/
+int detect_decision(int xOne, int yOne, int xTwo, int yTwo, Vec3b oneColor, Vec3b twoColor, Mat image)
+{
+    //check to make sure you remembered to do calibration step 2
+    if (vector_is_empty(oneColor) || vector_is_empty(twoColor))
+    {
+        cout << "not calibrated" << endl;
+        return -1;
+    }
+
+    //assign variables needed to check for color disparities
+    Vec3b& tmp1 = image.at<Vec3b>(yOne, xOne);
+    Vec3b& tmp2 = image.at<Vec3b>(yTwo, xTwo);
+    int threshold = 50;
+    bool selected1 = false;
+    bool selected2 = false;
+
+    //Checking for color disparities with option 1
+    Vec3b d = oneColor - tmp1;
+    //multiply each element by itself individually
+    d[0] = d[0] * d[0];
+    d[1] = d[1] * d[1];
+    d[2] = d[2] * d[2];
+
+    int difference = d[0] + d[1] + d[2]; //calculate total difference by adding RGB values together
+
+    if (difference > threshold)
+    {
+        selected1 = true;
+    }
+
+    //Checking for color disparities with option 2
+    d = twoColor - tmp1;
+    //multiply each element by itself individually
+    d[0] = d[0] * d[0];
+    d[1] = d[1] * d[1];
+    d[2] = d[2] * d[2];
+
+    difference = d[0] + d[1] + d[2];
+    if (difference > threshold)
+    {
+        selected2 = true;
+    }
+
+    //delete vectors to avoid memory leaks - before checking for decision or would never be deleted when decision is found
+    delete& tmp1;
+    delete& tmp2;
+    delete& d;
+
+    if (selected1 && selected2)
+    {
+        cout << "You must only choose 1 choice" << endl;
+        return -1;
+    }
+    else if (selected1)
+    {
+        cout << "Option 1 was chosen" << endl;
+        return 1;
+    }
+    else if (selected2)
+    {
+        cout << "Option 2 was chosen" << endl;
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+
+    return 0; //only gets here if no option chosen and exits if statement for some reason
+}
+
+//list all cameras since there are more than 1 - webcam + offboard
+//only need to run this method once at very beginning
+//my computer: port 0 is webcam and port 1 is external camera
+int list_ports()
+{
+    //create arrays of size bigger than possible number of ports
+    //must keep track of index for each array
+    int working[5] = { NULL, NULL, NULL, NULL, NULL };
+    int working_index = 0;
+
+    int non_working[6] = { NULL, NULL, NULL, NULL, NULL, NULL };
+    int non_working_index = 0;
+
+    int available[5] = { NULL, NULL, NULL, NULL, NULL };
+    int available_index = 0;
+
+    int port = 0;
+    VideoCapture camera;
+
+    //variables to hold data for when the port is working
+    Mat img;
+    bool is_reading = true;
+    double w = 0; double h = 0;
+
+    //tests until finds more than 6 non working ports - last spot in non_working array is filled
+    while (non_working[5] == NULL)
+    {
+        camera = VideoCapture(port);
+        if (!camera.isOpened())
+        {
+            non_working[non_working_index] = port;
+            cout << "Port " << port << " is not working" << endl;
+            non_working_index++;
+        }
+        else
+        {
+            is_reading = camera.read(img);
+            w = camera.get(3);
+            h = camera.get(4);
+
+            if (is_reading)
+            {
+                cout << "Port " << port << " is working and reads images (" << h << " x " << w << ")" << endl;
+                working[working_index] = port;
+                working_index++;
+            }
+            else
+            {
+                cout << "Port " << port << " for camera (" << h << " x " << w << ") is present but does not read" << endl;
+                available[available_index] = port;
+                available_index++;
+            }
+        }
+        camera.release(); //release the camera each time
+        port++; //move to the next port to be worked with
+    }
+    return 0;
+}
+
 int main()
 {
     //Find coordinates of both displays - laptop and monitor
@@ -67,6 +305,11 @@ int main()
             break;
         }
         imshow("camOut", frame);
+
+        //FOR CALIBRATION STEP 1
+        //getImage("Start_A_0001.jpg", wDisplay1);
+        //if (image_counter == 1) { getImage("Scene 1A Flowers_0015.jpg", wDisplay1); }
+        //if (image_counter == 2) { getImage("Scene 1B_0015.jpg", wDisplay1); }
 
         //calibrate automatically based on scene - only calibrate if it hasn't been done yet before
         if (currentScene == 0 && vector_is_empty(knobLeftColor) && vector_is_empty(knobRightColor))
@@ -188,242 +431,4 @@ int main()
     delete &springKeyColor;
     cam.release();
     destroyAllWindows();
-}
-
-/*
-* This method will be used to find the colors of the different decision options
-*/
-int calibrate(int xOne, int yOne, int xTwo, int yTwo, Vec3b& oneColor, Vec3b& twoColor, Mat image)
-{
-    //uses reference variables for  colors to edit the color variables in main method without returning multiple values
-    //assigns RGB value at location of decision to color vector
-    oneColor = image.at<Vec3b>(yOne, xOne);
-    twoColor = image.at<Vec3b>(yTwo, xTwo);
-    cout << "calibrate: " << oneColor << " and " << twoColor << endl;
-    return 1;
-}
-
-/*
-* This method checks if the vector is empty so that this code does not take up space in the main method each time it needs to be used
-*/
-bool vector_is_empty(Vec3b vector)
-{
-    if (vector[0] == 0)
-    {
-        if (vector[1] == 0)
-        {
-            if (vector[2] == 0)
-            {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-/*
-* This method will be used to find and display the image the program should be looking for.
-* This method should be called in the beginning for the start scene and for the pause scenes when waiting for user's decision
-* @args imageName = name of image file to look for with .jpg extension
-* @args wDisplay1
-*/
-int getImage(String imageName, int wDisplay1)
-{
-    //import image from file folder 
-    string image_path = samples::findFile(imageName); //FIX THIS FILE PATH
-    Mat img = imread(image_path, IMREAD_COLOR);
-    cout << img.size << endl;
-
-    //set height and width to cols and rows respectively
-    int hStory = img.cols;
-    int wStory = img.rows;
-
-    if (img.empty())
-    {
-        cout << "Could not read the image: " << image_path << endl;
-        return -1;
-    }
-
-    //display the image to the correct window
-    namedWindow("story", WINDOW_NORMAL);
-    moveWindow("story", wDisplay1, 0); //position display
-    setWindowProperty("story", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
-    imshow("story", img); //displays the image
-
-    delete &img; //delete to avoid memory leaks
-    return 0;
-}
-
-/*
-* This method will be used to display the video of the correct scene 
-*/
-int playVideo(String videoName, int wDisplay1)
-{
-    VideoCapture cap(videoName);
-    // Check if camera opened successfully
-    if (!cap.isOpened()) {
-        cout << "Error opening video stream or file" << endl;
-        return -1;
-    }
-
-    //display video to the correct window
-    namedWindow("video", WINDOW_NORMAL);
-    moveWindow("video", wDisplay1, 0); //position display
-    setWindowProperty("video", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
-    Mat frame;
-
-    while (1) 
-    {
-        // Capture frame-by-frame
-        cap >> frame;
-
-        // If the frame is empty, break immediately
-        if (frame.empty())
-            break;
-
-        // Display the resulting frame
-        imshow("video", frame);
-    }
-
-    delete &frame;
-    cap.release();
-    return 0;
-    //DO I NEED TO CALL GET IMAGE VARIABLE HERE
-}
-
-/*
-* This method will be used to detect if the user has made their decision in a given scene
-* @args xOne, yOne - the x and y coordinates of option 1
-* @args xTwo, yTwo - the x and y coordinates of option 2
-* @args oneColor, twoColor - the original colors of option 1 and 2 determined in calibration step 2
-* @args image - the current image displayed on the screen
-*/
-int detect_decision(int xOne, int yOne, int xTwo, int yTwo, Vec3b oneColor, Vec3b twoColor, Mat image)
-{
-    //check to make sure you remembered to do calibration step 2
-    if (vector_is_empty(oneColor) || vector_is_empty(twoColor))
-    {
-        cout << "not calibrated" << endl;
-        return -1;
-    }
-
-    //assign variables needed to check for color disparities
-    Vec3b& tmp1 = image.at<Vec3b>(yOne, xOne);
-    Vec3b& tmp2 = image.at<Vec3b>(yTwo, xTwo);
-    int threshold = 50;
-    bool selected1 = false;
-    bool selected2 = false;
-
-    //Checking for color disparities with option 1
-    Vec3b d = oneColor - tmp1;
-    //multiply each element by itself individually
-    d[0] = d[0] * d[0];
-    d[1] = d[1] * d[1];
-    d[2] = d[2] * d[2];
-
-    int difference = d[0] + d[1] + d[2]; //calculate total difference by adding RGB values together
-
-    if (difference > threshold)
-    {
-        selected1 = true;
-    }
-
-    //Checking for color disparities with option 2
-    d = twoColor - tmp1;
-    //multiply each element by itself individually
-    d[0] = d[0] * d[0];
-    d[1] = d[1] * d[1];
-    d[2] = d[2] * d[2];
-
-    difference = d[0] + d[1] + d[2];
-    if (difference > threshold)
-    {
-        selected2 = true;
-    }
-
-    //delete vectors to avoid memory leaks - before checking for decision or would never be deleted when decision is found
-    delete &tmp1;
-    delete &tmp2;
-    delete &d;
-
-    if (selected1 && selected2)
-    {
-        cout << "You must only choose 1 choice" << endl;
-        return -1;
-    }
-    else if (selected1)
-    {
-        cout << "Option 1 was chosen" << endl;
-        return 1;
-    }
-    else if (selected2)
-    {
-        cout << "Option 2 was chosen" << endl;
-        return 2;
-    }
-    else
-    {
-        return 0;
-    }
-
-    return 0; //only gets here if no option chosen and exits if statement for some reason
-}
-
-//list all cameras since there are more than 1 - webcam + offboard
-//only need to run this method once at very beginning
-//my computer: port 0 is webcam and port 1 is external camera
-int list_ports() 
-{
-    //create arrays of size bigger than possible number of ports
-    //must keep track of index for each array
-    int working[5] = { NULL, NULL, NULL, NULL, NULL };
-    int working_index = 0;
-
-    int non_working[6] = { NULL, NULL, NULL, NULL, NULL, NULL };
-    int non_working_index = 0;
-
-    int available[5] = { NULL, NULL, NULL, NULL, NULL };
-    int available_index = 0;
-
-    int port = 0;
-    VideoCapture camera;
-
-    //variables to hold data for when the port is working
-    Mat img;
-    bool is_reading = true;
-    double w = 0; double h = 0;
-    
-    //tests until finds more than 6 non working ports - last spot in non_working array is filled
-    while (non_working[5] == NULL)
-    {
-        camera = VideoCapture(port);
-        if (!camera.isOpened())
-        {
-            non_working[non_working_index] = port;
-            cout << "Port " << port << " is not working" << endl;
-            non_working_index++;
-        }
-        else
-        {
-            is_reading = camera.read(img);
-            w = camera.get(3);
-            h = camera.get(4);
-
-            if (is_reading)
-            {
-                cout << "Port " << port << " is working and reads images (" << h << " x " << w << ")" << endl;
-                working[working_index] = port;
-                working_index++;
-            }
-            else
-            {
-                cout << "Port " << port << " for camera (" << h << " x " << w << ") is present but does not read" << endl;
-                available[available_index] = port;
-                available_index++;
-            }
-        }
-        camera.release(); //release the camera each time
-        port++; //move to the next port to be worked with
-    }
-    return 0;
 }
